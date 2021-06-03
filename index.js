859a51be0158f190339745330dd1ae35f538a88b
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-03 12:00:13 -0600

    CRA and linked

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
 const server = express()
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build'))) // static assets
 
 console.log('howdy partner')
 console.log("USER----->", process.env.USER)
@@ -13,6 +19,10 @@ if (process.envNODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000
 
 console.log('port is ->', PORT)
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 
 server.get('/api', (req, res) => {
     res.json({ message: `${process.env.COHORT} ROCKS` })
